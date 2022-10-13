11abf25305c2308cb046649d0415269da22d5995
Daniel <daniel_sousa.p@outlook.com>
2022-10-13 01:07:27 -0300

    feat: create flags

--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+
+	"github.com/dannielss/go-github-cli/cmd/outputs"
+)
 
 func main() {
-	fmt.Println("First commit")
+	var h bool
+	var e bool
+	var user string
+
+	flag.StringVar(&user, "u", "", "")
+	flag.StringVar(&user, "user", "", "")
+
+	flag.BoolVar(&h, "h", false, "")
+	flag.BoolVar(&h, "help", false, "")
+
+	flag.BoolVar(&e, "e", false, "")
+	flag.BoolVar(&e, "export", false, "")
+
+	setFlag(flag.CommandLine)
+
+	flag.Parse()
+
+	if h {
+		outputs.ShowHelp()
+		return
+	}
+
+	outputs.GetHeader()
+
+	if user != "" {
+		outputs.GetRepositoriesInfo(user)
+		return
+	}
+
+	if e {
+		log.Printf("Exporting as csv...")
+	}
+}
+
+func setFlag(flag *flag.FlagSet) {
+	flag.Usage = func() {
+		outputs.ShowHelp()
+	}
 }
